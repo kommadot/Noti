1240a0accd14a2c5e9529f900b358e2378ce4e03
ssgkmd <tlagyqls7@naver.com>
2018-07-08 17:05:49 +0900

    dialog complete

--- app/src/main/java/com/example/simhyobin/noti/fragment_2.java
+++ app/src/main/java/com/example/simhyobin/noti/fragment_2.java
@@ -1,26 +1,31 @@
 package com.example.simhyobin.noti;
 
+import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 import android.support.v4.app.Fragment;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
+import java.util.Locale;
 
 public class fragment_2 extends Fragment {
     // Store instance variables
-    private String title;
-    private int page;
-    private String time;
+    private View views;
     // newInstance constructor for creating fragment with arguments
     public static fragment_2 newInstance(int page, String title) {
         fragment_2 fragmentFirst = new fragment_2();
@@ -41,33 +46,77 @@ public class fragment_2 extends Fragment {
                              Bundle savedInstanceState) {
         String sec=getString(R.string.secondpage);
         View view = inflater.inflate(R.layout.fragment_fragment_2, container, false);
+        views=view;
         TextView message = (TextView) view.findViewById(R.id.message);
-        view.findViewById(R.id.dateButton).setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
-                DialogFragment newFragment = new selectDateFragment();
-                newFragment.show(getActivity().getSupportFragmentManager(),"datePicker");
-            }
-        });
+        Calendar calendar = Calendar.getInstance(Locale.KOREA);
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+        final int minute = calendar.get(Calendar.MINUTE);
+        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        String time;
+        int h = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if (h > 12){
+            h=h-12;
+            time="오후 "+h+"시 "+minute+"분";
+        }
+        else {
+            time="오전 "+h+"시 "+minute+"분";
+        }
+        Button b = (Button) views.findViewById(R.id.timeButton);
+        b.setText(time);
+        String date;
+        int m =calendar.get(Calendar.MONTH);
+        m=m+1;
+        date=calendar.get(Calendar.YEAR)+"/"+m+"/"+calendar.get(Calendar.DAY_OF_MONTH);
+        Button bu = (Button) views.findViewById(R.id.dateButton);
+        bu.setText(date);
         view.findViewById(R.id.timeButton).setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
-                Bundle b = new Bundle();
-                DialogFragment newFragment = new SelectTimeFragment(b);
-                Bundle b;
-                newFragment.show(getActivity().getSupportFragmentManager(),"timePicker");
-                b=newFragment.getArguments();
-                SelectTimeFragment fragment = (SelectTimeFragment) getFragmentManager().findFragmentById(R.id.fragment_select_time);
-                int h;
-                int m;
-                h= b.getInt("hour");
-                m= b.getInt("minute");
-                time = h+"시 "+m+"분 ";
-                Log.d("komad",time);
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(),timeSetListener,hour,minute,false);
+
+                timePickerDialog.show();
             }
         });
-        Button b = (Button) view.findViewById(R.id.timeButton);
-        b.setText(time);
-        //message.setText(sec);
+        view.findViewById(R.id.dateButton).setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), dateSetListener, year, month, day);
+                datePickerDialog.show();
+            }
+        });
         return view;
     }
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            String date;
+            date=year+"/"+monthOfYear+"/"+dayOfMonth;
+            Button b = (Button) views.findViewById(R.id.dateButton);
+            b.setText(date);
+
+        }
+    };
+    private TimePickerDialog.OnTimeSetListener timeSetListener = new TimePickerDialog.OnTimeSetListener() {
+
+        @Override
+        public void onTimeSet(TimePicker view, int hour, int minute) {
+
+            String time;
+            if (hour > 12){
+                hour= hour-12;
+                time="오후 "+hour+"시 "+minute+"분";
+            }
+            else {
+                time="오전 "+hour+"시 "+minute+"분";
+            }
+            Button b = (Button) views.findViewById(R.id.timeButton);
+            b.setText(time);
+
+        }
+    };
+
 
 }
